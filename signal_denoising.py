92c26b44509ba2a0666b6429f5b5e6229a187110
Lukas Kemmer <lukaskemmer@Lukass-MacBook-Pro-3.local>
2018-05-31 16:07:05 +0200

    adds plots

--- signal_denoising.py
+++ signal_denoising.py
@@ -31,14 +31,20 @@ R = np.eye(n)[:-1,:] - np.column_stack((np.zeros((n-1,1)), np.eye(n-1)))
 b = y.reshape((n,m))
 A = np.eye(n)
 
+# Set plot style
+plt.style.use(['seaborn-talk'])
+
 for i, alpha in enumerate(alphas):
     print("Alpha: {}".format(alpha))
     # Calculate x_bar by solving (A^TA + alpha R^TR)x = A^Tb
     x_bar = np.linalg.solve(np.dot(A.T, A) + alpha*np.dot(R.T, R),
                             np.dot(A.T, b))
     # Plot result
+    plt.plot(t, y)
     plt.plot(t, x_bar)
     plt.title("Alpha={}".format(alpha))
+    plt.legend(['Original data (y)', 'Denoised signal (f(t))'])
+    plt.xlabel("t")
+    plt.ylabel("")
     plt.show()
-    
-    # We can see, that alpha controls the "smotheness" of the result
+    # We can see, that alpha controls the "smotheness" of the results
